65d1d3c3e00406c584a87ca1b3b99b4155f6a626
Dylan Compton <dylancomptontx@gmail.com>
2024-11-23 15:17:06 -0600

    potential cors fix 10

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 const whitelist = ['https://www.mybudgetree.com', 'http://localhost:5173']
 const corsOptions = {
