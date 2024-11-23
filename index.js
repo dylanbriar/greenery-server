492599991ec778bb99c44536cbecb609eec2b4ba
Dylan Compton <dylancomptontx@gmail.com>
2024-11-23 14:18:44 -0600

    potential cors fix 4

--- index.js
+++ index.js
@@ -4,21 +4,21 @@ const app = express();
 const cors = require('cors');
 const PORT = 3000; //letters?
 
-// const whitelist = ['https://www.mybudgetree.com', 'http://localhost:5173/']
-// const corsOptions = {
-//   origin: function (origin, callback) {
-//     if (whitelist.indexOf(origin) !== -1 || !origin) {
-//       callback(null, true)
-//     } else {
-//       callback(new Error('Not allowed by CORS'))
-//     }
-//   }
-// }
-
-var corsOptions = {
-  origin: 'https://mybudgetree.com',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+const whitelist = ['https://www.mybudgetree.com/', 'http://localhost:5173/']
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
 }
+
+// var corsOptions = {
+//   origin: 'https://mybudgetree.com',
+//   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+// }
 
 app.get(`/`, cors(corsOptions), (req, res) => {
   res.send({
